e75bb7fb55fab894c0735e0c52ab7b9158bf7064
Wouter Stinckens <wouter.stinckens@kbc.be>
2015-10-13 15:10:07 +0200

    adding bulk data, pagination and sorting

--- 09_userapi/server/user.js
+++ 09_userapi/server/user.js
@@ -1,6 +1,7 @@
 var _ = require('underscore');
 var express = require('express');
 var mongoose = require('mongoose');
+var faker = require('faker');
 var userMapper = require('./userMapper');
 
 var router = express.Router();
@@ -19,19 +20,65 @@ var User = mongoose.model('User', {
 	}
 });
 
+// init db
+var userCollection = [];
+_(1000).times(function() {
+	userCollection.push({
+		firstName: faker.name.firstName(),
+		lastName: faker.name.lastName(), 
+		age: faker.random.number(),
+		email: faker.internet.email(),
+		homeAddress: {
+			addressLine: faker.address.streetAddress(),
+			city: faker.address.city(),
+			zip: faker.address.zipCode()
+		}
+    });
+});
+User.collection.insert(userCollection, function(err, docs) {
+	if (err) {
+		console.log("Error while inserting records: " + err);
+	} 
+	else {
+		console.log("Succes, number of documents inserted: " + docs.insertedCount);
+	}
+
+});
+
 
 // setup services
 router.get('/', function(req, res, next) {
-	User.find(function(err, users) {
-		var formattedUsers = _.map(users, function(user) {
-			return userMapper.map(user);
+    var pageSize = 0;
+    if (req.query.pageSize) {
+    	pageSize = parseInt(req.query.pageSize);
+    }
+    var page = 0;
+    if (req.query.page) {
+    	page = parseInt(req.query.page);
+    }
+    var sort = '';
+    if (req.query.sort) {
+    	sort = req.query.sort;
+    }
+
+	User.find()
+		.limit(pageSize)
+		.skip(page * pageSize)
+		.sort(sort)
+		.exec(function(err, users) {
+			var formattedUsers = _.map(users, function(user) {
+				return userMapper.map(user);
+			});
+			return res.status(200).send(formattedUsers);
 		});
-		return res.status(200).send(formattedUsers);
-	});
 });
 
 router.get('/:id', function(req, res, next) {
 	User.findOne({_id: req.params.id}, function(err, user) {
+		if (err) {
+			return res.status(500).send('There is a problem: ' + err);
+		}
+
 		if (user) {
 			return res.status(200).send(userMapper.map(user));
 		}
@@ -55,7 +102,10 @@ router.post('/', function(req, res, next) {
 			zip: req.body.zip
 		}
     });
-    user.save(function() {
+    user.save(function(err) {
+    	if (err) {
+    		return res.status(500).send('We have a problem: ' + err);
+    	}
     	var formattedUser = userMapper.map(user);
     	return res.set('location', 'http://localhost:3000/api/users/' + formattedUser.id).status(201).send(formattedUser); 
     });
