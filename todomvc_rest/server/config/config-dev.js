2e4f414dfe47c20280bf43796a8e338e84de230a
Wouter Stinckens <wouter.stinckens@kbc.be>
2015-10-13 10:36:45 +0200

    adding config in separate files

---
+++ todomvc_rest/server/config/config-dev.js
@@ -0,0 +1,5 @@
+var config = require('./config-default');
+config.env = 'dev';
+config.port=4000;
+
+module.exports = config;
---
+++ todomvc_rest/server/config/config-tst.js
@@ -0,0 +1,5 @@
+var config = require('./config-default');
+config.env = 'tst';
+config.port=5000;
+
+module.exports = config;
